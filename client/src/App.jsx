8399e50d8e2e72041e6b2f7b0bfcc151ef6642fe
plla2 <jhbljs92@naver.com>
2023-06-15 16:40:52 +0900

    Feat: footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-let a= 344;
-
-let b=32;
+import './App.css';
+import Footer from './components/footer/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  let x=2;
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <span>test</span>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
