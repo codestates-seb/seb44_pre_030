0ee2521f61a5929202adc2e93258420f011289b1
plla2 <jhbljs92@naver.com>
2023-06-15 10:56:41 +0900

    Test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,6 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <span>test</span>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
