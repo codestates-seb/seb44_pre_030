4d195e990bc2da71b93d324344aeff298b781e92
yuheejoon <yuhetube4@gmail.com>
2023-06-15 10:46:06 +0900

    test-연결여부확인

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+let a= 0;
+let b= 0;
+
 function App() {
   const [count, setCount] = useState(0)
 
