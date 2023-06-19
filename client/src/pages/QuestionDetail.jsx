bcefb7a99d39323ed36b06de8b9f45995e17770b
parkchaeyeon <pcy6904@naver.com>
2023-06-19 22:54:31 +0900

    Refactor : header 리팩터

--- client/src/pages/QuestionDetail.jsx
+++ client/src/pages/QuestionDetail.jsx
@@ -3,7 +3,7 @@ import { styled } from 'styled-components';
 import ViewQuestionDetail from '../components/question/ViewQuestionDetail'
 import WriteAnswer from '../components/answer/WriteAnswer';
 import Answer from '../components/answer/Answer';
-import ViewQuestionDetail from '../components/question/ViewQuestionDetail';
+
 
 const QuestionDetail = () => {
   return (
