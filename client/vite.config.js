4542174500b0db663bc229a98d9d1fc30df6e6f2
parkchaeyeon <pcy6904@naver.com>
2023-06-22 16:00:43 +0900

    Feat : proxy 경로 추가

--- client/vite.config.js
+++ client/vite.config.js
@@ -19,6 +19,12 @@ export default defineConfig({
         secure: false,
         rewrite: (path) => path.replace(/^\/QuestionList/, ''),
       },
+      '/QuestionDetail':{
+        target:'http://ec2-13-209-35-180.ap-northeast-2.compute.amazonaws.com:8080/question/id',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/QuestionDetail\/\d+/, ''),
+      },
       '/answers': {
         target:
           'http://ec2-43-201-23-173.ap-northeast-2.compute.amazonaws.com:8080/answers',
