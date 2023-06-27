41ebd0ee1871d49b897f4ebd02c506d5e56848c9
plla2 <jhbljs92@naver.com>
2023-06-27 09:46:40 +0900

    API수정, 데이터속성 수정

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -33,51 +33,132 @@ function App() {
   const [location, setLocation] = useState(null);
   const [title, setTitle] = useState(null);
 
-  const [inputText,setInputText] = useState('');
-  const [enterState,setEnterState] = useState(false);
+  const [inputText, setInputText] = useState('');
+  const [enterState, setEnterState] = useState(false);
   useEffect(() => {
-    if(userInfo){
-    setProfile(userInfo.data.displayName);
-    setIsId(userInfo.data.id);
-    if(userInfo.data.location === null){
-    setLocation("");
-    }else{
-      setLocation(userInfo.data.location);
-    }
-    if(userInfo.data.title === null){
-      setTitle("");
-      }else{
+    if (userInfo) {
+      setProfile(userInfo.data.displayName);
+      setIsId(userInfo.data.id);
+      if (userInfo.data.location === null) {
+        setLocation('');
+      } else {
+        setLocation(userInfo.data.location);
+      }
+      if (userInfo.data.title === null) {
+        setTitle('');
+      } else {
         setTitle(userInfo.data.title);
       }
-    console.log(userInfo.data);
-    console.log(isId);
-    console.log(profile);
-    console.log(location);
+      console.log(userInfo.data);
+      console.log(isId);
+      console.log(profile);
+      console.log(location);
     }
-})
-
+  });
 
   return (
     <>
-      <Header isLogin={isLogin} setIsLogin={setIsLogin} setInputText={setInputText} setEnterState={setEnterState}/>
+      <Header
+        isLogin={isLogin}
+        setIsLogin={setIsLogin}
+        setInputText={setInputText}
+        setEnterState={setEnterState}
+      />
       <MainDiv>
-      <Sidebar />
-      <Routes>
-        <Route path="/" element={<QuestionList inputText={inputText} enterState={enterState} setEnterState={setEnterState} isLogin={isLogin}/>} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login setIsLogin={setIsLogin} setUserInfo={setUserInfo}/>} />
-        <Route path="/question/ask" element={<CreateQuestion isId={isId} isLogin={isLogin}/>} />
-        <Route path="/question/:id" element={<QuestionDetail/>} />
-        <Route path="/question/edit/:id" element={<EditQuestion />} />
-        <Route path="/answer/edit/:id" element={<EditAnswer />} />
-        <Route path="/mypage/:id" element={<MyPage setIsLogin={setIsLogin} isLogin={isLogin} profile={profile} location={location} title={title}/>} />
-        <Route path="/mypage/summary/:id" element={<MyPageSummary profile={profile} location={location} title={title}/>} />
-        <Route path="/mypage/answers/:id" element={<MyPageAnswers profile={profile} location={location} title={title}/>} />
-        <Route path="/mypage/questions/:id" element={<MyPageQuestions profile={profile} location={location} title={title}/>} />
-        <Route path="/mypage/tags/:id" element={<MyPageTags profile={profile} location={location} title={title}/>} />
-        <Route path="/mypage/edit/:id" element={<EditMyPage isLogin={isLogin} setIsLogin={setIsLogin} userInfo={userInfo} location={location} profile={profile} title={title} setUserInfo={setUserInfo}/>} />
-        <Route path="/tagslist" element={<TagsList />} />
-      </Routes>
+        <Sidebar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <QuestionList
+                inputText={inputText}
+                enterState={enterState}
+                setEnterState={setEnterState}
+                isLogin={isLogin}
+              />
+            }
+          />
+          <Route path="/signup" element={<Signup />} />
+          <Route
+            path="/login"
+            element={
+              <Login setIsLogin={setIsLogin} setUserInfo={setUserInfo} />
+            }
+          />
+          <Route
+            path="/question/ask"
+            element={<CreateQuestion isId={isId} isLogin={isLogin} />}
+          />
+          <Route
+            path="/question/:id"
+            element={<QuestionDetail isId={isId} />}
+          />
+          <Route path="/question/edit/:id" element={<EditQuestion />} />
+          <Route path="/answer/edit/:id" element={<EditAnswer />} />
+          <Route
+            path="/mypage/:id"
+            element={
+              <MyPage
+                setIsLogin={setIsLogin}
+                isLogin={isLogin}
+                profile={profile}
+                location={location}
+                title={title}
+              />
+            }
+          />
+          <Route
+            path="/mypage/summary/:id"
+            element={
+              <MyPageSummary
+                profile={profile}
+                location={location}
+                title={title}
+              />
+            }
+          />
+          <Route
+            path="/mypage/answers/:id"
+            element={
+              <MyPageAnswers
+                profile={profile}
+                location={location}
+                title={title}
+              />
+            }
+          />
+          <Route
+            path="/mypage/questions/:id"
+            element={
+              <MyPageQuestions
+                profile={profile}
+                location={location}
+                title={title}
+              />
+            }
+          />
+          <Route
+            path="/mypage/tags/:id"
+            element={
+              <MyPageTags profile={profile} location={location} title={title} />
+            }
+          />
+          <Route
+            path="/mypage/edit/:id"
+            element={
+              <EditMyPage
+                isLogin={isLogin}
+                setIsLogin={setIsLogin}
+                userInfo={userInfo}
+                location={location}
+                profile={profile}
+                title={title}
+                setUserInfo={setUserInfo}
+              />
+            }
+          />
+          <Route path="/tagslist" element={<TagsList />} />
+        </Routes>
       </MainDiv>
       <Footer />
     </>
